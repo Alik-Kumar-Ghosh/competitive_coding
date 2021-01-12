fd125586f0066da8d8ca20d0c9a647c830294e7f
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2021-01-12 20:18:16 +0530

    Add files via upload

---
+++ greeksforgreeks/Reverse_String.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    int last = str.length();
+    for (int i = 0; i < last / 2; i++)
+    swap(str[i], str[last - i - 1]);
+    return str;
+}
